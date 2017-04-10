42d3477a6a5b3f0e9872c23b39fe12f1cfdf062c
shivpratapsingh1 <rrpsingh16@gmail.com>
2017-04-10 09:09:32 +0530

    Add files via upload

---
+++ Q4/Happyfii.java
@@ -0,0 +1,9 @@
+public class Happyfii
+{
+	String boyfriend;String girlfriend;double happiness;
+	Happyfii(String boyfriend, String girlfriend, double happiness)
+        {
+		this.boyfriend = boyfriend;this.girlfriend = girlfriend;
+		this.happiness = happiness;
+	}
+}
---
+++ Q4/ReadMe.txt
@@ -0,0 +1,11 @@
+cd ppl-assignment-shivpratapsingh1
+cd Q4
+javac q1.java q2.java q4.java
+java q4
+cat log3.txt log1.txt log2.txt log4.txt log.txt
+javadoc *.java
+on the gitbash
+(Windows 10)
+64-bit
+Java SE 8
+Documentation done using javadocs.
